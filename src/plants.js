31759016506bf17950565e03d25dab3dafe75ea8
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-01 13:51:51 -0700

    Move comments to notes.js file, update plants.js.
    Co-authored-by: Thomas Glenn <glenaissance@yahoo.com>

--- src/plants.js
+++ src/plants.js
@@ -1,36 +1,3 @@
-// const hydrate = (plant) => { //function literal. "plant" argument
-//   return {
-//     ...plant, // spread operator to return the new state of the plant
-//     water: (plant.water || 0) + 1 //plant.water = NaN, needs to default to 0 if there is no water property.
-//   }
-// };
-
-// const feed = (plant) => {
-//   return {
-//     ...plant,
-//     soil: (plant.soil || 0) + 1
-//   }
-// };
-
-const changePlantState = (plant, property) => {
-  return {
-    ...plant,
-    [property]: (plant[property] || 0) + 1 //use bracket notation when we don't yet know the contents value.
-  }
-};
-
-const changeState = (state, prop) => {
-  return {
-    ...state,
-    [prop]: (state[prop] || 0) + 1
-  }
-};
-
-const changeState = (state, prop, value) => ({ //State is the object(?)
-  ...state,     //instatiation of the new object(?)
-  [prop] : (state[prop] || 0) + value
-});
-
 const changeState = (prop) => {
   return (value) => {
     return (state) => ({
@@ -40,72 +7,26 @@ const changeState = (prop) => {
   }
 };
 
-// const copyOfFunction = (prop) => {
-//   return (value) => {
-//     return (state) => ({
-//       ...state,
-//       [prop] : (state[prop] || 0) + value
-//     })
-//   }
-// };
-// const copyOfFunction = changeState(prop);
-// copyOfFunction(value)(state)
+const feed = changeState("soil");
+const hydrate = changeState("water");
+const giveLight = changeState("light");
 
-// const feed = changeState("soil"); // feed = changeState(value 0)(prop)(state 0)
-// const hydrate = changeState("water");
-// const giveLight = changeState("light");
-
-// const blueFood = changeState("soil")(5) //bluefood = changeState(5)(plant)(soil)
-// const greenFood = changeState("soil")(10)
-// const yuckyFood = changeState("soil")(-5)
+const blueFood = changeState("soil")(5)
+const greenFood = changeState("soil")(10)
+const yuckyFood = changeState("soil")(-5)
 
 // we have a _____ (plant/state) whose ____ (soil/prop) increases by _____ (5/value) when we _____ (feed = name of the function)
 
-// I have a ____(dog) whose ____ (happiness) increases by ____ (10) when we ____ (chase kitty cats)
-
-// I have a _____(car/state) whose _________ (motor/prop) decreases by _______ (10000/value) when I _______ (drive)
-
-const changeState = ("motor") => {
-  return (value) => {
-    return (state) => ({
-      ...state,
-      ["motor"] : (state["motor"] || 0) - value
-    })
+const storeState = () => { //no arg, only job is to store the current state in storeState - this outer function is the room 
+  let currentState = {}; // initialized as an empty object  - use let because this will mutate each time the inner function is called
+  return (stateChangeFunction) => {  // anonymous inner function that takes arg stateChangeFunction / takes func as arg
+    const newState = stateChangeFunction(currentState); // takes the function that's passed in, will call it on current state / newState holds the new state made from stateChangeFunction(currentState)
+    currentState = {...newState}; //makes a copy of newState and assigns it to currentstate
+    return newState;
   }
-};
+} 
 
-const driveCar = changeState("motor")
-driveCar(10000)(car)
-const dyingCar = driveCar(10000)
-
-
-
-changeState(motor)(1000)(car)
-
-
-
-
-
-
-
-
-// const changeDogMood = (prop) => {
-//     return (value) => {
-//       return (state) => ({
-//         ...state,
-//         [prop] : (state[prop] || 0) + value
-//       })
-//     }
-//   };
-
-//   const chaseKittyCats = ("happiness") => {
-//     return (10) => {
-//       return (dog) => ({
-//         ...dog,
-//         ["happiness"] : (state["happiness"] || 0) + 10
-//       })
-//     }
-//   };
-
-//   const chaseKittyCats = changeDogMood("happiness");
-//   chaseKittyCats(10)(dog);
+const stateControl = storeState(); //creates a closure over the currentState variable
+const fedPlant = stateControl(blueFood);
+console.log(fedPlant);
+// ... X  = this is a copy of X
